ec814fc3cfd53c4c1b42904a1a3f068dc938cbde
Karina Matushkina <k.matushkina@mcplat.ru>
2023-09-14 10:37:43 +0300

    functions 1-1

--- Basics/checkAge.js
+++ Basics/checkAge.js
@@ -20,10 +20,5 @@ function checkAge(age) {
 
 function checkAge(age) {
   return (age > 18) ? true : confirm('Родители разрешили?');
+  // return (age > 18) || confirm('Родители разрешили?');
 }
-
-/*
-function checkAge(age) {
-  return (age > 18) || confirm('Родители разрешили?');
-}
-*/
