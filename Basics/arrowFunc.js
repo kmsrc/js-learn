12c9770a34b00d6aed8628de5c7dcc878ba7deee
Karina Matushkina <k.matushkina@mcplat.ru>
2023-09-15 07:32:00 +0300

    functions arrow

---
+++ Basics/arrowFunc.js
@@ -0,0 +1,13 @@
+// Замените код Function Expression стрелочной функцией:
+'use strict';
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  () => alert("Вы согласились."),
+  () => alert("Вы отменили выполнение.")
+);
